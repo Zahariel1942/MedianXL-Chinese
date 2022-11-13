80a58818cd5836a286d17450db2135920fe7b669
Naka <nakating@gmail.com>
2022-11-13 12:51:42 +0800

    update config and scripts

--- pack.sh
+++ pack.sh
@@ -1,7 +1,7 @@
 # /bin/bash
 
 rm -rf .tmp
-mkdir .tmp
+rm -rf dest
 
 collectOutput() {
 mv d2tbl_ouput.tbl .tmp/$1.tbl
@@ -14,6 +14,7 @@ collectOutput PatchString
 tools/d2tbl.exe -import src/CHI/ExpansionString.txt -ansi -always-insert
 collectOutput ExpansionString
 
+mkdir .tmp
 cp origin/lang-zh.mpq .tmp
 
 tools/MPQEditor.exe delete .tmp/lang-zh.mpq "data\local\LNG\CHI\String.tbl"
@@ -21,5 +22,5 @@ tools/MPQEditor.exe delete .tmp/lang-zh.mpq "data\local\LNG\CHI\PatchString.tbl"
 tools/MPQEditor.exe delete .tmp/lang-zh.mpq "data\local\LNG\CHI\ExpansionString.tbl"
 tools/MPQEditor.exe add .tmp/lang-zh.mpq ".tmp/*.tbl" "data\local\LNG\CHI" /auto
 
-rm -rf dest/lang-zh.mpq
+mkdir dest
 mv .tmp/lang-zh.mpq dest
